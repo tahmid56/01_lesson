b59488a1c685c1be95a176ce1393cd349f59b5d6
Tahmid Akter <tahmidakter56@gmail.com>
2022-10-21 01:37:53 +0600

    Counter App done

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { Store } from "./app/store";
+import { store } from "./app/store";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store = {store}>
       <App />
     </Provider>
   </React.StrictMode>
